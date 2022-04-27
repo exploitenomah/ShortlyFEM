42a0a0ca63c57e869097e290d3c9c6ff4503ac26
exploitenomah <iksploiting@gmail.com>
2022-04-27 11:10:32 +0100

    fix wrong import of next/image

--- components/HomeComponents/StatsCard.tsx
+++ components/HomeComponents/StatsCard.tsx
@@ -1,5 +1,5 @@
 
-import Image from 'next/Image'
+import Image from 'next/image'
 import { HeadingThree } from '../Headings'
 import { PBasic } from '../Paragraphs'
 import { stat } from '../../types/advancedStatsType'
